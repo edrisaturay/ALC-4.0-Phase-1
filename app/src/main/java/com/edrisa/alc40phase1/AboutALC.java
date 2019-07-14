960b5738b6cd1df39d2553f5c9c0f17a01c2be67
Edrisa A. Turay <eturay@iom.int>
2019-07-14 20:56:05 +0200

    Added my photo.. webview still not loading andela but loading google

--- app/src/main/java/com/edrisa/alc40phase1/AboutALC.java
+++ app/src/main/java/com/edrisa/alc40phase1/AboutALC.java
@@ -2,19 +2,39 @@ package com.edrisa.alc40phase1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class AboutALC extends AppCompatActivity {
 
     private WebView mAboutALCWebView;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_alc);
 
         mAboutALCWebView = findViewById(R.id.wv_about_alc);
-        mAboutALCWebView.loadUrl("https://andela.com/alc/");
+        mAboutALCWebView.getSettings().setLoadsImagesAutomatically(true);
+        mAboutALCWebView.getSettings().setJavaScriptEnabled(true);
+        mAboutALCWebView.getSettings().setLoadWithOverviewMode(true);
+        mAboutALCWebView.getSettings().setDomStorageEnabled(true);
+        mAboutALCWebView.getSettings().setUseWideViewPort(true);
+
+        mAboutALCWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        mAboutALCWebView.loadUrl("https://google.com");
+
+//        mAboutALCWebView.loadUrl("231shop.com");
     }
 }
--- app/src/main/java/com/edrisa/alc40phase1/MainActivity.java
+++ app/src/main/java/com/edrisa/alc40phase1/MainActivity.java
@@ -1,18 +1,37 @@
 package com.edrisa.alc40phase1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mProfile, mAboutALC;
+    private static final int MY_PERMISSIONS_REQUEST_INTERNET = 0000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.INTERNET)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.INTERNET)) {
+            } else {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.INTERNET},
+                        MY_PERMISSIONS_REQUEST_INTERNET);
+            }
+        }else{
+            Toast.makeText(MainActivity.this, "Internet Permission is Granted", Toast.LENGTH_LONG).show();
+        }
 
         mProfile = findViewById(R.id.btn_my_profile);
         mAboutALC = findViewById(R.id.btn_about);
@@ -31,4 +50,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_INTERNET: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    Toast.makeText(MainActivity.this, "Please allow internet permission. We will need it to display the WebView.", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
+    }
+
 }
