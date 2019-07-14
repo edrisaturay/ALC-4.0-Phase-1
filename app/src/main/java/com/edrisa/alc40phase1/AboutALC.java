57686b87e16aaa7d020f176b80fcd16d4eb9e2a0
Edrisa A. Turay <eturay@iom.int>
2019-07-14 21:29:03 +0200

    Fully Working Application according to the instructions

--- app/src/main/java/com/edrisa/alc40phase1/AboutALC.java
+++ app/src/main/java/com/edrisa/alc40phase1/AboutALC.java
@@ -3,7 +3,11 @@ package com.edrisa.alc40phase1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.net.http.SslError;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.ClientCertRequest;
+import android.webkit.SslErrorHandler;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
@@ -19,7 +23,7 @@ public class AboutALC extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_alc);
 
-        mAboutALCWebView = findViewById(R.id.wv_about_alc);
+        mAboutALCWebView = (WebView) findViewById(R.id.wv_about_alc);
         mAboutALCWebView.getSettings().setLoadsImagesAutomatically(true);
         mAboutALCWebView.getSettings().setJavaScriptEnabled(true);
         mAboutALCWebView.getSettings().setLoadWithOverviewMode(true);
@@ -32,9 +36,20 @@ public class AboutALC extends AppCompatActivity {
                 view.loadUrl(url);
                 return true;
             }
-        });
-        mAboutALCWebView.loadUrl("https://google.com");
 
-//        mAboutALCWebView.loadUrl("231shop.com");
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                Log.i("Page Loaded", "True");
+            }
+        });
+
+        mAboutALCWebView.loadUrl("https://andela.com/alc/");
+
+
     }
 }
